5fafa08c56925874add34a1dc57552352c88a7e9
fsologureng <fsologur@uchile.cl>
2024-08-29 11:08:24 -0400

    ci: modernizes test wf - centralized ci. - updates coverage badge.

--- .github/test.sh
+++ .github/test.sh
@@ -1,20 +1,21 @@
 #!/bin/dash
 
-pip install -e /openedx/requirements/uchileedxlogin
+set -e
 
-cd /openedx/requirements/uchileedxlogin
+pip install --src /openedx/venv/src -e /openedx/requirements/app
+pip install pytest-cov genbadge[coverage]
+
+cd /openedx/requirements/app
 cp /openedx/edx-platform/setup.cfg .
+sed -i '/--json-report/c addopts = --nomigrations --reuse-db --durations=20 --json-report --json-report-omit keywords streams collectors log traceback tests --json-report-file=none --cov=uchileedxlogin/ --cov-report term-missing --cov-report xml:reports/coverage/coverage.xml --cov-fail-under 70' setup.cfg
+
 mkdir test_root
 cd test_root/
 ln -s /openedx/staticfiles .
 
-cd /openedx/requirements/uchileedxlogin
-
-pip install pytest-cov genbadge[coverage]
-sed -i '/--json-report/c addopts = --nomigrations --reuse-db --durations=20 --json-report --json-report-omit keywords streams collectors log traceback tests --json-report-file=none --cov=uchileedxlogin/ --cov-report term-missing --cov-report xml:reports/coverage/coverage.xml --cov-fail-under 70' setup.cfg
+cd /openedx/requirements/app
 
 DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest uchileedxlogin/tests.py
 
-rm -rf test_root
-
+echo "[run]\nomit = uchileedxlogin/migrations/*" > .coveragerc
 genbadge coverage
