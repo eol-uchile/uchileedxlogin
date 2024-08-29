23a4a8ab829fb81768416b3e0f0a26356a22a82b
fsologureng <fsologur@uchile.cl>
2024-08-29 11:13:03 -0400

    chore: updates setup metadata

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ import setuptools
 setuptools.setup(
     name="uchileedxlogin",
     version="0.0.1",
-    author="Luis Santana",
-    author_email="luis.santana@uchile.cl",
+    author="EOL Uchile",
+    author_email="eol-ing@uchile.cl",
     description="Authentication backend for EOL from UChile api and Enroll/Unenroll/Export users",
     long_description="Authentication backend for EOL from UChile api and Enroll/Unenroll/Export users",
     url="https://eol.uchile.cl",
     packages=setuptools.find_packages(),
     install_requires=["unidecode>=1.1.1"],
     classifiers=[
-        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
